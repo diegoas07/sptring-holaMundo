cf8f518b056cc3f5553aabd12fa10c6d87faacd6
Desarrollo2 <devsolutionCo@gmail.com>
2018-10-24 11:00:42 -0500

    re

---
+++ src/main/java/beans/BeanSpring.java
@@ -0,0 +1,17 @@
+package beans;
+
+public class BeanSpring {
+
+	private String _mensaje;
+
+	public String get_mensaje() {
+		return _mensaje;
+	}
+
+	public void set_mensaje(String _mensaje) {
+		this._mensaje = _mensaje;
+	}
+	
+	
+	
+}
